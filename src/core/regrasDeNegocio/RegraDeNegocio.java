c62e2ca0ee1a63d490ed9942a1f242111d1a7c2e
Vitor Cosso <vitorcosso@gmail.com>
2023-11-26 12:42:38 -0300

    feat: Adicionando classe Service

--- src/core/regrasDeNegocio/RegraDeNegocio.java
+++ src/core/regrasDeNegocio/RegraDeNegocio.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import core.Cliente;
 import core.Empresa;
+import core.services.EmpresaService;
 import infra.Produto;
 import infra.Usuario;
 import infra.Venda;
@@ -37,48 +38,12 @@ public class RegraDeNegocio {
 
 					switch (escolha) {
 					case 1: {
-						System.out.println();
-						System.out.println("************************************************************");
-						System.out.println("VENDAS EFETUADAS");
-						vendas.stream().forEach(venda -> {
-							if (venda.getEmpresa().getId().equals(usuarioLogado.getEmpresa().getId())) {
-								System.out.println("************************************************************");
-								System.out.println("Venda de código: " + venda.getCódigo() + " no CPF "
-										+ venda.getCliente().getCpf() + ": ");
-								venda.getItens().stream().forEach(x -> {
-									System.out.println(x.getId() + " - " + x.getNome() + "    R$" + x.getPreco());
-								});
-								System.out.println("Total Venda: R$" + venda.getValor());
-								System.out.println("Total Taxa a ser paga: R$" + venda.getComissaoSistema());
-								System.out.println("Total Líquido  para empresa: R$ " +  usuarioLogado.getEmpresa().getSaldo());
-								System.out.println("************************************************************");
-							}
-
-						});
-						System.out.println("Saldo Empresa: " + usuarioLogado.getEmpresa().getSaldo());
-						System.out.println("************************************************************");
-
+                        EmpresaService.listarVendas(usuarioLogado, vendas);
 						executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
                         break;
 					}
 					case 2: {
-						System.out.println();
-						System.out.println("************************************************************");
-						System.out.println("MEUS PRODUTOS");
-						produtos.stream().forEach(produto -> {
-							if (produto.getEmpresa().getId().equals(usuarioLogado.getEmpresa().getId())) {
-								System.out.println("************************************************************");
-								System.out.println("Código: " + produto.getId());
-								System.out.println("Produto: " + produto.getNome());
-								System.out.println("Quantidade em estoque: " + produto.getQuantidade());
-								System.out.println("Valor: R$" + produto.getPreco());								
-								System.out.println("************************************************************");
-							}
-
-						});
-						System.out.println("Saldo Empresa: " + usuarioLogado.getEmpresa().getSaldo());
-						System.out.println("************************************************************");
-
+                        EmpresaService.verProdutos(usuarioLogado, produtos);
 						executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
                         break;
 					}
---
+++ src/core/services/EmpresaService.java
@@ -0,0 +1,56 @@
+package core.services;
+
+import java.util.List;
+
+import infra.Produto;
+import infra.Usuario;
+import infra.Venda;
+
+public class EmpresaService {
+    
+    public static void verProdutos(Usuario usuarioLogado, List<Produto> produtos){
+        System.out.println();
+        System.out.println("************************************************************");
+        System.out.println("MEUS PRODUTOS");
+        produtos.stream().forEach(produto -> {
+            if (produto.getEmpresa().getId().equals(usuarioLogado.getEmpresa().getId())) {
+                System.out.println("************************************************************");
+                System.out.println("Código: " + produto.getId());
+                System.out.println("Produto: " + produto.getNome());
+                System.out.println("Quantidade em estoque: " + produto.getQuantidade());
+                System.out.println("Valor: R$" + produto.getPreco());								
+                System.out.println("************************************************************");
+            }
+
+        });
+        System.out.println("Saldo Empresa: " + usuarioLogado.getEmpresa().getSaldo());
+        System.out.println("************************************************************");
+
+    }
+
+
+    public static void listarVendas(Usuario usuarioLogado, List<Venda> vendas){
+        System.out.println();
+        System.out.println("************************************************************");
+        System.out.println("VENDAS EFETUADAS");
+        vendas.stream().forEach(venda -> {
+            if (venda.getEmpresa().getId().equals(usuarioLogado.getEmpresa().getId())) {
+                System.out.println("************************************************************");
+                System.out.println("Venda de código: " + venda.getCódigo() + " no CPF "
+                        + venda.getCliente().getCpf() + ": ");
+                venda.getItens().stream().forEach(x -> {
+                    System.out.println(x.getId() + " - " + x.getNome() + "    R$" + x.getPreco());
+                });
+                System.out.println("Total Venda: R$" + venda.getValor());
+                System.out.println("Total Taxa a ser paga: R$" + venda.getComissaoSistema());
+                System.out.println("Total Líquido  para empresa: R$ " +  usuarioLogado.getEmpresa().getSaldo());
+                System.out.println("************************************************************");
+            }
+
+        });
+        System.out.println("Saldo Empresa: " + usuarioLogado.getEmpresa().getSaldo());
+        System.out.println("************************************************************");
+    }
+
+
+}
